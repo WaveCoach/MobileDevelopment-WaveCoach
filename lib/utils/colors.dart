2fb48f7248ca82fe573411a3d50021660f5d88db
cintarmynti <ramayanticinta@gmail.com>
2025-03-16 17:32:40 +0700

    setup material theme font textstyle and color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color backgroundColor = Color(0xFFF1FAFF);
 Color blueColor = Color(0xFF3E6585);
@@ -6,3 +7,12 @@ Color circleBarBlueColor = Color(0xFF89B5D9);
 Color fadeBlueColor = Color(0xFF9EB2C2);
 Color greyColor = Color.fromARGB(255, 48, 48, 48);
 Color fadeYellowColor = Color(0xFFEDC892);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: Colors.black);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(color: Colors.white);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
