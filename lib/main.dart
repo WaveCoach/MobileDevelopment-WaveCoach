7287ecf2ed8bd0cb26be3b00dd2a1d08f42b298e
cintarmynti <ramayanticinta@gmail.com>
2025-03-17 13:59:44 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:wavecoach/views/forgot_password.dart';
+import 'package:wavecoach/views/home.dart';
+import 'package:wavecoach/views/onboarding_page.dart';
+import 'package:wavecoach/views/sign_in_page.dart';
 import 'package:wavecoach/views/splash_screen_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'login',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-      ),
-      home: SplashScreenPage(),
+      routes: {
+        '/': (context) => SplashScreenPage(),
+        '/onboarding': (context) => OnboardingPage(),
+        '/sign-in': (context) => SignInPage(),
+        '/home': (context) => HomeScreenView(),
+        '/forgot-password': (context) => ForgotPasswordView(),
+      },
     );
   }
 }
