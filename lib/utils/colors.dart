3988aa50bda9384f1f2da34771646cb0a1f9c0c4
cintarmynti <ramayanticinta@gmail.com>
2025-03-17 13:53:11 +0700

    fix colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30.0;
 
 Color backgroundColor = Color(0xFFF1FAFF);
 Color blueColor = Color(0xFF3E6585);
@@ -8,5 +11,17 @@ Color greyColor = Color.fromARGB(255, 48, 48, 48);
 Color fadeYellowColor = Color(0xFFEDC892);
 Color blackBlueColor = Color(0xFF223340);
 Color boxJadwal = Color(0xFFC1D2DD);
-Color waktuBoxJadwal = Color (0xFFECC791);
+Color waktuBoxJadwal = Color(0xFFECC791);
 Color reschedule = Color(0xFFE2A7A8);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: Colors.black);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(color: Colors.white);
+TextStyle grayTextStyle = GoogleFonts.poppins(
+  color: Colors.white.withOpacity(0.5),
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
