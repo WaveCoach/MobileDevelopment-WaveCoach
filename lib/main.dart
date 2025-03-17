d195a96af18af0ea2b2f23fa2be557e7707bf2f2
cintarmynti <ramayanticinta@gmail.com>
2025-03-17 14:03:26 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:wavecoach/views/forgot_password.dart';
-import 'package:wavecoach/views/home.dart';
+import 'package:wavecoach/views/splash_screen_page.dart';
 import 'package:wavecoach/views/onboarding_page.dart';
 import 'package:wavecoach/views/sign_in_page.dart';
-import 'package:wavecoach/views/splash_screen_page.dart';
+import 'package:wavecoach/views/home.dart';
+import 'package:wavecoach/views/forgot_password.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Login',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+      ),
+      initialRoute: '/', // Menentukan rute awal
       routes: {
         '/': (context) => SplashScreenPage(),
         '/onboarding': (context) => OnboardingPage(),
